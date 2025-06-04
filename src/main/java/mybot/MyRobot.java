d1d0c1c3e7cbda53cb4fe3778b380743567c46af
jchyczynski <jasiek.ch16@gmail.com>
2025-06-05 00:57:28 +0200

    WIP use qlearning library

--- src/main/java/mybot/MyRobot.java
+++ src/main/java/mybot/MyRobot.java
@@ -1,43 +1,67 @@
 package mybot;
 
-import robocode.AdvancedRobot;
-import robocode.ScannedRobotEvent;
+import robocode.*;
+import org.deeplearning4j.rl4j.network.dqn.DQNFactoryStdDense;
+import org.deeplearning4j.rl4j.network.configuration.DQNFactoryStdDense.Configuration;
+import org.deeplearning4j.rl4j.policy.DQNPolicy;
+import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearningDiscreteDense;
+import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearning;
+import org.deeplearning4j.rl4j.environment.Environment;
+import org.deeplearning4j.rl4j.environment.StepResult;
+import org.deeplearning4j.rl4j.environment.EnvironmentStep;
+import org.deeplearning4j.rl4j.space.Observation;
+import org.deeplearning4j.rl4j.space.DiscreteSpace;
+import org.deeplearning4j.rl4j.environment.EnvironmentListener;
+import org.nd4j.linalg.learning.config.Adam;
 
-// DL4J imports
-import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
-import org.deeplearning4j.nn.conf.layers.DenseLayer;
-import org.deeplearning4j.nn.conf.layers.OutputLayer;
-import org.nd4j.linalg.activations.Activation;
-import org.nd4j.linalg.lossfunctions.LossFunctions;
+import java.util.*;
 
 public class MyRobot extends AdvancedRobot {
+
+    private SimpleGunEnv env;
+    private QLearningDiscreteDense<double[]> dql;
+
     @Override
     public void run() {
-        // Simple DL4J NN configuration example
-        NeuralNetConfiguration.ListBuilder builder = new NeuralNetConfiguration.Builder()
-                .list()
-                .layer(new DenseLayer.Builder()
-                        .nIn(5)
-                        .nOut(10)
-                        .activation(Activation.RELU)
-                        .build())
-                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
-                        .activation(Activation.IDENTITY)
-                        .nIn(10)
-                        .nOut(1)
-                        .build());
+        env = new SimpleGunEnv(this);
 
-        System.out.println("Created a simple DL4J neural network config");
+        Configuration netConf = DQNFactoryStdDense.Configuration.builder()
+                .l2(0.001).updater(new Adam(0.001))
+                .numHiddenNodes(32)
+                .numLayer(2)
+                .build();
+
+        QLearning.QLConfiguration rlConf = new QLearning.QLConfiguration(
+                123,    // Random seed
+                200,    // Max steps per episode
+                5000,   // Max episodes
+                200,    // Exp replay size
+                32,     // Batch size
+                500,    // Target update (steps)
+                0.99,   // Gamma
+                1.0,    // Initial epsilon
+                0.1,    // Min epsilon
+                1000    // Exploration anneal steps
+        );
+
+        dql = new QLearningDiscreteDense<>(env, netConf, rlConf);
 
         while (true) {
-//            ahead(100);
-            turnGunRight(360);
-            execute();
+            dql.train();
+            env.reset();
         }
     }
 
-    @Override
-    public void onScannedRobot(ScannedRobotEvent event) {
-        fire(1);
+    public void onScannedRobot(ScannedRobotEvent e) {
+        env.setEnemy(e);
+    }
+
+    // Optional: reward logic after a hit or miss
+    public void onBulletHit(BulletHitEvent e) {
+        env.reward(1.0);
+    }
+
+    public void onBulletMissed(BulletMissedEvent e) {
+        env.reward(-0.2);
     }
 }
