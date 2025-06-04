af70efa9e1ea9b314925f336934892b0a5153d41
jchyczynski <jasiek.ch16@gmail.com>
2025-06-05 01:38:00 +0200

    running with RL4J but not using it

--- src/main/java/mybot/MyRobot.java
+++ src/main/java/mybot/MyRobot.java
@@ -2,66 +2,40 @@ package mybot;
 
 import robocode.*;
 import org.deeplearning4j.rl4j.network.dqn.DQNFactoryStdDense;
-import org.deeplearning4j.rl4j.network.configuration.DQNFactoryStdDense.Configuration;
-import org.deeplearning4j.rl4j.policy.DQNPolicy;
-import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearningDiscreteDense;
-import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearning;
-import org.deeplearning4j.rl4j.environment.Environment;
-import org.deeplearning4j.rl4j.environment.StepResult;
-import org.deeplearning4j.rl4j.environment.EnvironmentStep;
-import org.deeplearning4j.rl4j.space.Observation;
+//import org.deeplearning4j.rl4j.learning.sync.qlearning.QLearningDiscreteDense;
+import org.deeplearning4j.rl4j.learning.sync.qlearning.discrete.QLearningDiscreteDense;
+import org.deeplearning4j.rl4j.mdp.MDP;
 import org.deeplearning4j.rl4j.space.DiscreteSpace;
-import org.deeplearning4j.rl4j.environment.EnvironmentListener;
+import org.deeplearning4j.rl4j.policy.DQNPolicy;
 import org.nd4j.linalg.learning.config.Adam;
 
-import java.util.*;
+import java.awt.Color;
 
 public class MyRobot extends AdvancedRobot {
 
-    private SimpleGunEnv env;
-    private QLearningDiscreteDense<double[]> dql;
-
     @Override
     public void run() {
-        env = new SimpleGunEnv(this);
+        setBodyColor(Color.BLUE);
+        setGunColor(Color.BLACK);
+        setRadarColor(Color.YELLOW);
+        setScanColor(Color.RED);
 
-        Configuration netConf = DQNFactoryStdDense.Configuration.builder()
-                .l2(0.001).updater(new Adam(0.001))
-                .numHiddenNodes(32)
+        out.println("Initializing DQN...");
+
+        // Just instantiating RL4J classes to verify setup
+        DQNFactoryStdDense.Configuration netConf = DQNFactoryStdDense.Configuration.builder()
+                .l2(0.001)
+                .updater(new Adam(0.001))
+                .numHiddenNodes(16)
                 .numLayer(2)
                 .build();
 
-        QLearning.QLConfiguration rlConf = new QLearning.QLConfiguration(
-                123,    // Random seed
-                200,    // Max steps per episode
-                5000,   // Max episodes
-                200,    // Exp replay size
-                32,     // Batch size
-                500,    // Target update (steps)
-                0.99,   // Gamma
-                1.0,    // Initial epsilon
-                0.1,    // Min epsilon
-                1000    // Exploration anneal steps
-        );
+        out.println("RL4J DQN config instantiated OK");
 
-        dql = new QLearningDiscreteDense<>(env, netConf, rlConf);
-
+        // Dummy loop
         while (true) {
-            dql.train();
-            env.reset();
+            turnGunRight(10); // just to show something
+            execute();
         }
-    }
-
-    public void onScannedRobot(ScannedRobotEvent e) {
-        env.setEnemy(e);
-    }
-
-    // Optional: reward logic after a hit or miss
-    public void onBulletHit(BulletHitEvent e) {
-        env.reward(1.0);
-    }
-
-    public void onBulletMissed(BulletMissedEvent e) {
-        env.reward(-0.2);
     }
 }
