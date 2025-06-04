b1fdf45b03df8519e0b5ffe12a712d979ac22b52
jchyczynski <jasiek.ch16@gmail.com>
2025-06-04 23:07:08 +0200

    basic_dl4_robot

--- src/main/java/mybot/MyRobot.java
+++ src/main/java/mybot/MyRobot.java
@@ -2,76 +2,42 @@ package mybot;
 
 import robocode.AdvancedRobot;
 import robocode.ScannedRobotEvent;
-import mylib.MathHelper;
 
-//import static jdk.javadoc.internal.tool.Main.execute;
+// DL4J imports
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 public class MyRobot extends AdvancedRobot {
+    @Override
     public void run() {
+        // Simple DL4J NN configuration example
+        NeuralNetConfiguration.ListBuilder builder = new NeuralNetConfiguration.Builder()
+                .list()
+                .layer(new DenseLayer.Builder()
+                        .nIn(5)
+                        .nOut(10)
+                        .activation(Activation.RELU)
+                        .build())
+                .layer(new OutputLayer.Builder(LossFunctions.LossFunction.MSE)
+                        .activation(Activation.IDENTITY)
+                        .nIn(10)
+                        .nOut(1)
+                        .build());
+
+        System.out.println("Created a simple DL4J neural network config");
+
         while (true) {
-            double angle = MathHelper.normalizeAngle(getHeading() + 10);
-            setTurnRight(angle);
             ahead(100);
+            turnGunRight(360);
             execute();
         }
     }
 
+    @Override
     public void onScannedRobot(ScannedRobotEvent event) {
         fire(1);
     }
 }
-
-
-//import robocode.*;
-////import java.awt.Color;
-//
-//// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-//
-///**
-// * MyRobot - a robot by (your name here)
-// */
-//public class MyRobot extends Robot
-//{
-//    /**
-//     * run: MyRobot's default behavior
-//     */
-//    public void run() {
-//        // Initialization of the robot should be put here
-//
-//        // After trying out your robot, try uncommenting the import at the top,
-//        // and the next line:
-//
-//        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-//
-//        // Robot main loop
-//        //noinspection InfiniteLoopStatement
-//        while (true) {
-//            ahead(200);
-//            turnGunRight(360);
-//            turnLeft(120);
-//        }
-//    }
-//
-//    /**
-//     * onScannedRobot: What to do when you see another robot
-//     */
-//    public void onScannedRobot(ScannedRobotEvent e) {
-//        // Replace the next line with any behavior you would like
-//        fire(1);
-//    }
-//
-//    /**
-//     * onHitByBullet: What to do when you're hit by a bullet
-//     */
-//    public void onHitByBullet(HitByBulletEvent e) {
-//        back(100);
-//    }
-//
-//    /**
-//     * onHitWall: What to do when you hit a wall
-//     */
-//    public void onHitWall(HitWallEvent e) {
-//        // Replace the next line with any behavior you would like
-//        back(20);
-//    }
-//}
