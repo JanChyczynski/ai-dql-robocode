9b1abb85125e594d9e65c8a8c9cb45abb344fc49
jchyczynski <jasiek.ch16@gmail.com>
2025-06-08 16:44:43 +0200

    basic q learning

--- src/main/java/mybot/MyRobot.java
+++ src/main/java/mybot/MyRobot.java
@@ -1,77 +1,123 @@
 package mybot;
 
-import robocode.AdvancedRobot;
-import robocode.ScannedRobotEvent;
-import mylib.MathHelper;
+import robocode.*;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Arrays;
 
 //import static jdk.javadoc.internal.tool.Main.execute;
 
 public class MyRobot extends AdvancedRobot {
+    private static final int NUM_DISTANCE_BUCKETS = 5;
+    private static final int NUM_BEARING_BUCKETS = 8;
+    private static final int NUM_ACTIONS = 5;
+
+    private static final double ALPHA = 0.1;
+    private static final double GAMMA = 0.9;
+    private static final double EPSILON = 0.1;
+
+    private double[][][] qTable = new double[NUM_DISTANCE_BUCKETS][NUM_BEARING_BUCKETS][NUM_ACTIONS];
+    private int prevDistance, prevBearing, prevAction;
+    private boolean hasPrevState = false;
+    private final String Q_TABLE_FILE = "qtable.data";
+
+    @Override
     public void run() {
+        loadQTable();
+        setAdjustGunForRobotTurn(true);
+        setAdjustRadarForGunTurn(true);
+
         while (true) {
-            double angle = MathHelper.normalizeAngle(getHeading() + 10);
-            setTurnRight(angle);
-            ahead(100);
-            execute();
+            turnRadarRight(360);
         }
     }
 
-    public void onScannedRobot(ScannedRobotEvent event) {
-        fire(1);
+    @Override
+    public void onScannedRobot(ScannedRobotEvent e) {
+        int dist = (int) Math.min(NUM_DISTANCE_BUCKETS - 1, e.getDistance() / (1000 / NUM_DISTANCE_BUCKETS));
+        int bearing = (int) Math.floor((e.getBearing() + 180) / (360.0 / NUM_BEARING_BUCKETS));
+
+        int action = chooseAction(dist, bearing);
+        performAction(action);
+
+        double reward = 0;
+        if (e.getEnergy() < 20) reward += 1;
+        if (getEnergy() < 20) reward -= 1;
+
+        if (hasPrevState) {
+            double oldQ = qTable[prevDistance][prevBearing][prevAction];
+            double maxQ = Arrays.stream(qTable[dist][bearing]).max().orElse(0);
+            qTable[prevDistance][prevBearing][prevAction] = oldQ + ALPHA * (reward + GAMMA * maxQ - oldQ);
+        }
+
+        prevDistance = dist;
+        prevBearing = bearing;
+        prevAction = action;
+        hasPrevState = true;
+
+        scan();
+    }
+
+    private int chooseAction(int dist, int bearing) {
+        if (Math.random() < EPSILON) {
+            return (int)(Math.random() * NUM_ACTIONS);
+        }
+        return maxQIndex(dist, bearing);
+    }
+
+    private int maxQIndex(int dist, int bearing) {
+        double[] q = qTable[dist][bearing];
+        int best = 0;
+        for (int i = 1; i < q.length; i++) {
+            if (q[i] > q[best]) best = i;
+        }
+        return best;
+    }
+
+    private void performAction(int action) {
+        switch (action) {
+            case 0: setAhead(100); break;
+            case 1: setBack(100); break;
+            case 2: setTurnRight(45); break;
+            case 3: setTurnLeft(45); break;
+            case 4: setFire(1.5); break;
+        }
+        execute();
+    }
+
+    @Override
+    public void onWin(WinEvent e) {
+        out.println("Victory! Saving Q-table...");
+        saveQTable();
+    }
+
+    @Override
+    public void onDeath(DeathEvent e) {
+        out.println("Defeated. Saving Q-table...");
+        saveQTable();
+    }
+
+    private void saveQTable() {
+        try (ObjectOutputStream out = new ObjectOutputStream(new RobocodeFileOutputStream(getDataFile(Q_TABLE_FILE)))) {
+            out.writeObject(qTable);
+            out.flush();
+        } catch (IOException ex) {
+            out.println("Failed to save Q-table: " + ex.getMessage());
+        }
+    }
+
+    private void loadQTable() {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(getDataFile(Q_TABLE_FILE)))) {
+            Object obj = in.readObject();
+            if (obj instanceof double[][][]) {
+                qTable = (double[][][]) obj;
+                out.println("Loaded Q-table.");
+            }
+        } catch (IOException | ClassNotFoundException ex) {
+            out.println("No existing Q-table found, starting fresh.");
+        }
     }
 }
-
-
-//import robocode.*;
-////import java.awt.Color;
-//
-//// API help : https://robocode.sourceforge.io/docs/robocode/robocode/Robot.html
-//
-///**
-// * MyRobot - a robot by (your name here)
-// */
-//public class MyRobot extends Robot
-//{
-//    /**
-//     * run: MyRobot's default behavior
-//     */
-//    public void run() {
-//        // Initialization of the robot should be put here
-//
-//        // After trying out your robot, try uncommenting the import at the top,
-//        // and the next line:
-//
-//        // setColors(Color.red,Color.blue,Color.green); // body,gun,radar
-//
-//        // Robot main loop
-//        //noinspection InfiniteLoopStatement
-//        while (true) {
-//            ahead(200);
-//            turnGunRight(360);
-//            turnLeft(120);
-//        }
-//    }
-//
-//    /**
-//     * onScannedRobot: What to do when you see another robot
-//     */
-//    public void onScannedRobot(ScannedRobotEvent e) {
-//        // Replace the next line with any behavior you would like
-//        fire(1);
-//    }
-//
-//    /**
-//     * onHitByBullet: What to do when you're hit by a bullet
-//     */
-//    public void onHitByBullet(HitByBulletEvent e) {
-//        back(100);
-//    }
-//
-//    /**
-//     * onHitWall: What to do when you hit a wall
-//     */
-//    public void onHitWall(HitWallEvent e) {
-//        // Replace the next line with any behavior you would like
-//        back(20);
-//    }
-//}
